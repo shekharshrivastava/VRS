a0eb722ddf6b62642748fb6001e905be3669d7ca
shekharshrivastava <ershrivastavashekhar@gmail.com>
2018-03-02 18:25:12 +0530

    Firebase authentication

--- app/src/main/java/com/app/ssoft/vrs/View/LoginActivity.java
+++ app/src/main/java/com/app/ssoft/vrs/View/LoginActivity.java
@@ -1,8 +1,9 @@
 package com.app.ssoft.vrs.View;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,12 +11,18 @@ import android.widget.Toast;
 
 import com.app.ssoft.vrs.R;
 import com.app.ssoft.vrs.Utils.Utils;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText etUserName;
     private EditText etPassword;
     private Button btnLogin;
+    private Button btnRegister;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,23 +31,56 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         etUserName = findViewById(R.id.etUserName);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
         btnLogin.setOnClickListener(this);
+        btnRegister.setOnClickListener(this);
+        auth = FirebaseAuth.getInstance();
     }
 
     @Override
     public void onClick(View v) {
-        String userID = etUserName.getText().toString();
-        String password = etPassword.getText().toString();
-        if (!userID.isEmpty() && !password.isEmpty()) {
-            if (Utils.validEmail(userID)) {
+        switch (v.getId()) {
+            case R.id.btnLogin:
+                String userID = etUserName.getText().toString();
+                final String password = etPassword.getText().toString();
+                if (!userID.isEmpty() && !password.isEmpty()) {
+                    if (Utils.validEmail(userID)) {
+                        auth.signInWithEmailAndPassword(userID, password)
+                                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        // If sign in fails, display a message to the user. If sign in succeeds
+                                        // the auth state listener will be notified and logic to handle the
+                                        // signed in user can be handled in the listener.
+//                                    progressBar.setVisibility(View.GONE);
+                                        if (!task.isSuccessful()) {
+                                            // there was an error
+                                            if (password.length() < 6) {
+//                                            inputPassword.setError(getString(R.string.minimum_password));
+                                            } else {
+                                                Toast.makeText(LoginActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+                                            }
+                                        } else {
+                                            auth.getCurrentUser();
+                                            Intent intent = new Intent(LoginActivity.this, VehicleDetailsActivity.class);
+                                            startActivity(intent);
+                                            finish();
+                                        }
+                                    }
+                                });
 
-            } else {
-                Toast.makeText(this, R.string.email_error_msg, Toast.LENGTH_SHORT).show();
-            }
-        } else {
-            Intent intent = new Intent(this,VehicleDetailsActivity.class);
-            startActivity(intent);
-            Toast.makeText(this, "Email/Password required", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(this, R.string.email_error_msg, Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Intent intent = new Intent(this, VehicleDetailsActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(this, "Email/Password required", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.btnRegister:
+                Intent intent = new Intent(this, RegistrationActivity.class);
+                startActivity(intent);
         }
     }
 }
